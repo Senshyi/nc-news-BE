1ebb2f819f7addc055cd686fb179d45dc3342f45
JanMach97 <janmachacek97@gmail.com>
2018-09-06 09:46:36 +0100

    add cors middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const apiRouter = require('./router/api');
+var cors = require('cors');
 const DB_URL = process.env.DB_URL || require('./config/db-config.js');
 
  app.use(bodyParser.json());
@@ -11,11 +12,13 @@ mongoose.connect(DB_URL, { useNewUrlParser: true })
     console.log(`Connected to ${DB_URL}`);
   });
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+  app.use(cors())
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
   app.use('/api', apiRouter);
 
